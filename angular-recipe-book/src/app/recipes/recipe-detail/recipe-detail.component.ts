8e33a39e5845a9a333e0d4a0e264b1e777fc9c7f
DimitarToshevski <dimitar_toshevski@abv.bg>
2018-07-28 12:48:29 +0300

    added new recipe route and edit recipe route and edit recipe component

--- angular-recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ angular-recipe-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -14,6 +14,7 @@ export class RecipeDetailComponent implements OnInit {
   id: number;
 
   constructor(private recipeService: RecipeService,
+              private router: Router,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -28,4 +29,7 @@ export class RecipeDetailComponent implements OnInit {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
 
+  onEditRecipe() {
+    this.router.navigate(['edit'], { relativeTo: this.route });
+  }
 }
