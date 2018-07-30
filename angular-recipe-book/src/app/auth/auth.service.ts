c44211b31b9c3df6afc64b695be9b32b1a65219e
DimitarToshevski <dimitar_toshevski@abv.bg>
2018-07-30 21:41:41 +0300

    added auth guard, enabling new and edit routes to authenticated users

--- angular-recipe-book/src/app/auth/auth.service.ts
+++ angular-recipe-book/src/app/auth/auth.service.ts
@@ -1,8 +1,14 @@
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 
+@Injectable()
 export class AuthService {
   token: string;
+
+  constructor(private router: Router) {}
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .catch(
@@ -13,6 +19,7 @@ export class AuthService {
   signinUser(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password)
     .then(response => {
+      this.router.navigate(['/']);
       firebase.auth().currentUser.getIdToken()
         .then( token => this.token = token);
     })
