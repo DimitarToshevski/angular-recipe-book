e44890987c709f30c3d8f5723d539834dfe03562
DimitarToshevski <dimitar_toshevski@abv.bg>
2018-07-30 20:50:19 +0300

    added signin, getting the token and appending to auth queryparam, saving and fetching data from firebase only if authenticated

--- angular-recipe-book/src/app/auth/auth.service.ts
+++ angular-recipe-book/src/app/auth/auth.service.ts
@@ -2,10 +2,28 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 
 export class AuthService {
+  token: string;
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .catch(
         error => console.log(error)
       );
+  }
+
+  signinUser(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(response => {
+      firebase.auth().currentUser.getIdToken()
+        .then( token => this.token = token);
+    })
+    .catch(
+      error => console.log(error)
+    );
+  }
+
+  getToken() {
+    firebase.auth().currentUser.getIdToken()
+        .then( token => this.token = token);
+        return this.token;
   }
 }
