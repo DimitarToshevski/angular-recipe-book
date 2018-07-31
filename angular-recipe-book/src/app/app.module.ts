c0f7a741370244a6c8b790296948d5b9487c63bf
DimitarToshevski <dimitar_toshevski@abv.bg>
2018-07-31 16:11:20 +0300

    added AoT build prod

--- angular-recipe-book/src/app/app.module.ts
+++ angular-recipe-book/src/app/app.module.ts
@@ -4,32 +4,26 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeService } from './recipes/recipe.service';
-import { DataStorageService } from './shared/data-storage.service';
-import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
-import { AuthGuard } from './auth/auth-guard.service';
+import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    AuthModule,
     AppRoutingModule,
+    BrowserModule,
+    CoreModule,
     FormsModule,
     HttpClientModule,
     SharedModule,
     ShoppingListModule,
-    AuthModule,
-    CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
